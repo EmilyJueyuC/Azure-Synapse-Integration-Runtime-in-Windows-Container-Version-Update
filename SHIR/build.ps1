d1310df6125a1213617e70108dd149acca46c069
EmilyJueyuC <Emily.Chen@au.logicalis.com>
2025-05-21 15:39:47 +1000

    Update Dockerfile to use Windows Server Core 2022 and install Microsoft OpenJDK 17 LTS

--- SHIR/build.ps1
+++ SHIR/build.ps1
@@ -38,8 +38,8 @@ function Install-SHIR() {
 function Install-MSFT-JDK() {
     Write-Log "Install the Microsoft OpenJDK in the Windows container"
 
-    Write-Log "Downloading Microsoft OpenJDK 11 LTS msi"
-    $JDKMsiFileName = 'microsoft-jdk-11-windows-x64.msi'
+    Write-Log "Downloading Microsoft OpenJDK 17 LTS msi"
+    $JDKMsiFileName = 'microsoft-jdk-17-windows-x64.msi'
 
     # Temporarily disable progress updates to speed up the download process. (See https://stackoverflow.com/questions/69942663/invoke-webrequest-progress-becomes-irresponsive-paused-while-downloading-the-fil)
     $ProgressPreference = 'SilentlyContinue'
